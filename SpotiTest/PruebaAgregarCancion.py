70c499037fb1a21608fb68eba78ca95984365a6f
OsvaldoLlNava <luis_llanesn@hotmail.com>
2019-10-29 15:23:34 -0600

    modificacion

--- SpotiTest/PruebaAgregarCancion.py
+++ SpotiTest/PruebaAgregarCancion.py
@@ -10,9 +10,11 @@ clientSecret='087a97142beb44a18e818424ae2b444c'
 token = util.prompt_for_user_token(username,scope=scopes,client_id=clientId,client_secret=clientSecret,redirect_uri='http://google.com/')
 sp = spotipy.Spotify(auth=token)
 playlist_id = '1AjVQ5uoae04O8bqsvjCMI'
-tracks_ids = 
-results = sp.user_playlist_add_tracks(username, playlist_id, track_ids)
+tracks_ids = '7KhsUnH6UelT8Sw6Uu29m7'
+results = sp.user_playlist_add_tracks(username, playlist_id, tracks_ids)
 print(results)
 #results = sp.current_user_playlists()
 
-#1AjVQ5uoae04O8bqsvjCMI
+#ID playlist = 1AjVQ5uoae04O8bqsvjCMI
+
+#ID cancion afrodita = 7KhsUnH6UelT8Sw6Uu29m7
