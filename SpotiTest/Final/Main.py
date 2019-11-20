492084bbc1b0f88d65fe046c9e9af8aed23f403a
Osvaldo Nava <luis_llanesn@hotmail.com>
2019-11-20 12:01:55 -0600

    asd

--- SpotiTest/Final/Main.py
+++ SpotiTest/Final/Main.py
@@ -1,19 +1,19 @@
 # import Syncro
 from Spoty.Track import Track
 import LeerCredenciales
-# from Spoty import Spoty
+from Spoty import Spoty
 
 if __name__ == '__main__':
     # credenciales = LeerCredenciales.Leer_Credenciales('Credenciales.txt')
     # # print(credenciales)
-    # username ='31hzoj7ckzb2dgudlq5zfbudzoia'
-    # clientId='005e25714bad49ab93e394adeabaaa96'
-    # clientSecret='087a97142beb44a18e818424ae2b444c'
+    username ='31hzoj7ckzb2dgudlq5zfbudzoia'
+    clientId='005e25714bad49ab93e394adeabaaa96'
+    clientSecret='087a97142beb44a18e818424ae2b444c'
     # # syn = Syncro.syncro((credenciales[0], credenciales[1], credenciales[2]))
 
     # # syn.Search_Track('rain', 'khan')
 
-    # sp = Spoty.Spoty(username, clientId, clientSecret)
-    # sp.Search_Track('rain', 'khan')
+    sp = Spoty.Spoty(username, clientId, clientSecret)
+    sp.Search_Track('rain', 'khan')
     can = Track('123','name','artist', 'album', '1.24')
     print(can)
